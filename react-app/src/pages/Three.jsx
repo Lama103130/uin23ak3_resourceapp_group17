4a347c6da821badeb08e784f1cb112a9bfe0b0f6
RamarrZanerni <rama198259@gmail.com>
2024-03-21 20:22:06 +0100

    delivery

---
+++ react-app/src/pages/Three.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ResourceList from '../component/ResourceList';
+
+const Three = () => {
+  return <ResourceList category="javascript" />;
+};
+
+export default Three;
