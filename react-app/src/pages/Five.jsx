2bc02ccbd53446200a07f429d1dc044257abf5db
RamarrZanerni <rama198259@gmail.com>
2024-03-21 16:45:24 +0100

    components

---
+++ react-app/src/pages/Five.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ResourceList from '../component/ResourceList';
+
+const Five = () => {
+  return <ResourceList category="headless-cms" />;
+};
+
+export default Five;
---
+++ react-app/src/pages/Four.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ResourceList from '../component/ResourceList';
+
+const Four = () => {
+  return <ResourceList category="react" />;
+};
+
+export default Four;
